1b5a66acd6edbc673587bca0525e5b7d2253c2f5
Milan Choudhary <choudharymilan08@gmail.com>
2026-01-19 15:05:47 +0530

    StudentModel and Home files are updated

--- src/main/java/com/example/revision/controller/Home.java
+++ src/main/java/com/example/revision/controller/Home.java
@@ -1,7 +1,9 @@
 package com.example.revision.controller;
 
+import com.example.revision.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Home {
@@ -9,6 +11,12 @@ public class Home {
     @GetMapping("/About")
     public String about() {
         return "This is about page";
+    }
+
+    @GetMapping("/")
+    StudentModel student() {
+        StudentModel student = new StudentModel(1,"Milan","mach.stephenson01@gmail.com");
+        return student;
     }
 
 }
--- src/main/java/com/example/revision/model/StudentModel.java
+++ src/main/java/com/example/revision/model/StudentModel.java
@@ -7,7 +7,9 @@ public class StudentModel {
     private String email;
 
     public StudentModel(int id, String name, String email) {
-
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
 
     public int getId() {
